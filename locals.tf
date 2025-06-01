1e24d3e670eb5b4d7f70277b0bb5a4963e4fe195
SiliconBorn <nevilleblast2238@gmail.com>
2025-06-01 21:38:46 +0530

    feat: setup

--- locals.tf
+++ locals.tf
@@ -12,5 +12,13 @@ locals {
       project_id          = module.project["fem-eci-project"].id
       vcs_repo_identifier = "SiliconBorn/fem-eci-terraform-tfe"
     }
+
+    "fem-eci-github" = {
+      description         = "Example description of the GitHub workspace"
+      execution_mode      = "local"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
+    }
   }
+
 }
